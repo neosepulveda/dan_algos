a8c154ea2503e329867316eb96a7bfe7fe5945ce
Neo Sepulveda <israel.sepulveda.castillejo@gmail.com>
2017-04-30 22:54:20 +0100

    Reading from STDIN

--- week1/roman.rb
+++ week1/roman.rb
@@ -1,6 +1,6 @@
 TO_ROMAN_CONVERSION = { 1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I" }
 
-TO_DECIMAL_CONVERSION = { "CM" => 900, "CD" => 400, "XC" => 90, "XL" => 40, "IX" => 9, "M" => 1000, "D" => 500, "C" => 100, "L" => 50, "X" => 10, "V" => 5, "IV" => 4, "I" => 1 }
+TO_DECIMAL_CONVERSION = { "CM" => 900, "CD" => 400, "XC" => 90, "XL" => 40, "IX" => 9, "IV" => 4, "M" => 1000, "D" => 500, "C" => 100, "L" => 50, "X" => 10, "V" => 5, "I" => 1 }
 
 def to_roman(decimal)
   return '' if decimal < 1
@@ -16,5 +16,13 @@ def to_decimal(roman)
   pair.last + to_decimal((r))
 end
 
-input = ARGV[0].dup
-puts "#{input.to_i != 0 ? to_roman(input.to_i) : to_decimal(input)}"
+if $0 == __FILE__
+  while line = gets
+    string = line.chomp
+    if string =~ /\A\d+\Z/
+      puts to_roman(string.to_i)
+    else
+      puts to_decimal(string)
+    end
+  end
+end
