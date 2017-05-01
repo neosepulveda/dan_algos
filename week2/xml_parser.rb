f58bfbb7a26ee8d7dc3aa2f6f3fe1a697bf9d7cc
Neo Sepulveda <israel.sepulveda.castillejo@gmail.com>
2017-05-01 22:53:28 +0100

    library of songs and an xml parser to output them as hash

---
+++ week2/xml_parser.rb
@@ -0,0 +1,36 @@
+require 'nokogiri'
+require 'ostruct'
+require 'pry'
+
+class XmlParser
+
+  def self.to_hash(xml_file)
+    library = Hash.new { |h, k| h[k] = [] }
+    doc = read_xml(xml_file)
+    doc.xpath("//Artist").each do |xml_artist|
+      artist_name = xml_artist.attributes["name"].value
+      xml_artist.children.each do |xml_song|
+        if xml_song.class == Nokogiri::XML::Element
+          song = parse_song(xml_song, artist_name)
+          library[song.name[0].downcase] << song
+        end
+      end
+    end
+    library
+  end
+
+  private
+
+  def self.parse_song(xml_song, artist)
+    OpenStruct.new(id: xml_song.attributes["id"].value,
+                   name: xml_song.attributes["name"].value,
+                   artist: artist,
+                   duration: xml_song.attributes["duration"].value.to_i)
+  end
+
+  def self.read_xml(xml_file)
+    Nokogiri::XML(File.open(xml_file))
+  end
+end
+
+#XmlParser.to_hash(ARGV[0])
